0660d43944cbb1f9b0925e692994c9ee1a6b836c
Toni Rämö <toni.ramo@helsinki.fi>
2020-08-09 21:56:26 +0300

    Add schema.sql

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    first_name TEXT,
+    last_name TEXT
+);
+
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
+    description TEXT,
+    teacher_id INTEGER REFERENCES users (id)
+);
+
+CREATE TABLE participants (
+    course_id INTEGER REFERENCES courses (id),
+    user_id INTEGER REFERENCES users (id)
+);
